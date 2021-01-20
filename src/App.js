2e4b7e0cd87ca301b0518ad40862bbf8611c3b33
Eli <elize.san10@gmail.com>
2021-01-20 16:11:34 -0600

    -data from nasa is fetched by using axios -effect hook added to handle API call side effect

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
+import axios from "axios";
+import { BASE_URL, API_KEY } from "./constants";
 
 function App() {
+  const [nasaData, setNASAData] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(`${BASE_URL}?api_key=${API_KEY}`)
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) =>
+        console.log(err, "Error in the nasa api, check nasaaxios")
+      );
+  }, []);
+
   return (
     <div className="App">
       <p>
         Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
+        app! Have fun{" "}
+        <span role="img" aria-label="go!">
+          🚀
+        </span>
+        !
       </p>
     </div>
   );
