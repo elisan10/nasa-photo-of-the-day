7efb43f96def026cd6103e4f81ae76a815c7b123
Eli <elize.san10@gmail.com>
2021-01-20 17:30:18 -0600

    -added constants file to gitignore -testing data rendering successful

--- src/App.js
+++ src/App.js
@@ -4,21 +4,25 @@ import axios from "axios";
 import { BASE_URL, API_KEY } from "./constants";
 
 function App() {
-  const [nasaData, setNASAData] = useState([]);
+  const [nasaData, setNASAData] = useState(null);
 
   useEffect(() => {
     axios
       .get(`${BASE_URL}?api_key=${API_KEY}`)
       .then((res) => {
-        console.log(res);
+        setNASAData(res.data);
       })
       .catch((err) =>
         console.log(err, "Error in the nasa api, check nasaaxios")
       );
   }, []);
 
+  console.log("this is nasa data", nasaData);
+  if (!nasaData) return <h3>Loading...</h3>;
+
   return (
     <div className="App">
+      <h1>{nasaData.title}</h1>
       <p>
         Read through the instructions in the README.md file to build your NASA
         app! Have fun{" "}
